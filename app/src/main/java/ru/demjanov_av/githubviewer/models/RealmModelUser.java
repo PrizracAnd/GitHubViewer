454a689cfc17a35d71d2cc2e4806dedeb2276134
PrizracAnd <prizracand@gmail.com>
2018-10-18 08:20:18 +0300

    Transit different.

--- app/src/main/java/ru/demjanov_av/githubviewer/models/RealmModelUser.java
+++ app/src/main/java/ru/demjanov_av/githubviewer/models/RealmModelUser.java
@@ -1,9 +1,11 @@
 package ru.demjanov_av.githubviewer.models;
 
 import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
 
 public class RealmModelUser extends RealmObject{
     private String login;
+    @PrimaryKey
     private String id;
     private String avatarUrl;
 
