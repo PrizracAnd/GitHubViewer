9dc9bb9c4edfd6d5d482001b8072500c7a1eb8dd
PrizracAnd <prizracand@gmail.com>
2018-11-29 21:17:26 +0300

    Edited RSA class to normal class

--- app/src/main/java/ru/demjanov_av/githubviewer/crypto/RSA.java
+++ app/src/main/java/ru/demjanov_av/githubviewer/crypto/RSA.java
@@ -1,36 +1,202 @@
 package ru.demjanov_av.githubviewer.crypto;
 
+import android.support.annotation.Nullable;
+import android.util.Log;
+
 import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+
 import java.security.*;
 
 public class RSA {
-    public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-        String s = "Hello world";
-        System.out.println(s + "\n");
+    //-----Constants begin-------------------------------
+    public final static String NAME_OF_RSA = "RSA";
+    public final static int DEFAULT_KEY_SIZE = 768;
+    //-----Constants end---------------------------------
 
-        Cipher cipher = Cipher.getInstance("RSA");
 
-        /* Генерация пары ключей */
-        KeyPairGenerator pairGenerator = KeyPairGenerator.getInstance("RSA");
+    //-----Class variables begin-------------------------
+    private Cipher cipher;
+
+    private Key publicKey;
+    private Key privateKey;
+
+    //-----Class variables end---------------------------
+
+
+    //////////////////////////////////////////////////////////
+    ///  Constructors
+    /////////////////////////////////////////////////////////
+    //-----Constructors begin-------
+    public RSA( ) {
+        try {
+            this.cipher = Cipher.getInstance(NAME_OF_RSA);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            Log.d(NAME_OF_RSA, ": " + e.getMessage());
+        }
+    }
+
+    public RSA(Key publicKey, Key privateKey) {
+        this.publicKey = publicKey;
+        this.privateKey = privateKey;
+
+        try {
+            this.cipher = Cipher.getInstance(NAME_OF_RSA);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            Log.d(NAME_OF_RSA, ": " + e.getMessage());
+        }
+    }
+    //-----Constructors end-------
+
+
+    //////////////////////////////////////////////////////////
+    ///  Methods generateKeys
+    /////////////////////////////////////////////////////////
+    //-----Begin--------------------
+    void generateKeys(){
+        KeyPairGenerator pairGenerator = null;
+        try {
+            pairGenerator = KeyPairGenerator.getInstance(NAME_OF_RSA);
+        } catch (NoSuchAlgorithmException e) {
+            Log.d(NAME_OF_RSA, ": " + e.getMessage());
+        }
 
         SecureRandom random = new SecureRandom();
-        pairGenerator.initialize(768, random);
+        try {
+            if(pairGenerator != null) {
+                pairGenerator.initialize(DEFAULT_KEY_SIZE, random);
+                KeyPair keyPair = pairGenerator.generateKeyPair();
+                this.publicKey = keyPair.getPublic();
+                this.privateKey = keyPair.getPrivate();
 
-        KeyPair keyPair = pairGenerator.generateKeyPair();
-        Key publicKey = keyPair.getPublic();
-        Key privateKey = keyPair.getPrivate();
-
-        /* Зашифрование и вывод */
-        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        byte[] bytes = cipher.doFinal(s.getBytes());
-        for (byte b : bytes) System.out.print(b);
-
-        /* Расшифрование и вывод */
-        Cipher decryptCipher = Cipher.getInstance("RSA");
-        decryptCipher.init(Cipher.DECRYPT_MODE, privateKey);
-        byte[] bytes2 = decryptCipher.doFinal(bytes);
-        System.out.println("\n");
-        for (byte b : bytes2) System.out.print((char)b);
-
+            }else {
+                Log.d(NAME_OF_RSA, ": " + "No generated keys!!!");
+            }
+        }catch (NullPointerException | InvalidParameterException e){
+            Log.d(NAME_OF_RSA, ": " + e.getMessage());
+        }
     }
+
+    void generateKeys(int keySize){
+        KeyPairGenerator pairGenerator = null;
+        try {
+            pairGenerator = KeyPairGenerator.getInstance(NAME_OF_RSA);
+        } catch (NoSuchAlgorithmException e) {
+            Log.d(NAME_OF_RSA, ": " + e.getMessage());
+        }
+
+        SecureRandom random = new SecureRandom();
+        try {
+            if(pairGenerator != null) {
+                pairGenerator.initialize(keySize, random);
+                KeyPair keyPair = pairGenerator.generateKeyPair();
+                this.publicKey = keyPair.getPublic();
+                this.privateKey = keyPair.getPrivate();
+            }else {
+                Log.d(NAME_OF_RSA, ": " + "No generated keys!!!");
+            }
+        }catch (NullPointerException | InvalidParameterException e){
+            Log.d(NAME_OF_RSA, ": " + e.getMessage());
+        }
+    }
+    //-----End----------------------
+
+
+    //////////////////////////////////////////////////////////
+    ///  Methods getKeys and setKeys
+    /////////////////////////////////////////////////////////
+    //-----Begin--------------------
+    @Nullable
+    public Key[] getKeys(){
+        Key[] keys = null;
+        if(this.publicKey != null && this.privateKey != null) {
+            keys = new Key[2];
+            keys[0] = this.publicKey;
+            keys[1] = this.privateKey;
+        }
+        return keys;
+    }
+
+    public void setKeys(Key publicKey, Key privateKey ){
+        this.publicKey = publicKey;
+        this.privateKey = privateKey;
+    }
+
+    @Nullable
+    public Key getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(Key publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    @Nullable
+    public Key getPrivateKey() {
+        return privateKey;
+    }
+
+    public void setPrivateKey(Key privateKey) {
+        this.privateKey = privateKey;
+    }
+    //-----End----------------------
+
+
+    //////////////////////////////////////////////////////////
+    ///  Method encrypt
+    /////////////////////////////////////////////////////////
+    public byte[] encrypt (byte[] openText, Key key){
+        byte[] bytes = new byte[0];
+
+        try {
+            this.cipher.init(Cipher.ENCRYPT_MODE, key);
+            bytes = this.cipher.doFinal(openText);
+        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+            Log.d(NAME_OF_RSA, ": " + e.getMessage());
+        }
+
+        return bytes;
+    }
+
+
+    //////////////////////////////////////////////////////////
+    ///  Method decrypt
+    /////////////////////////////////////////////////////////
+    public byte[] decrypt (byte[] encryptText, Key key){
+        byte[] bytes = new byte[0];
+
+        try {
+            this.cipher.init(Cipher.DECRYPT_MODE, key);
+            bytes = this.cipher.doFinal(encryptText);
+        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+            Log.d(NAME_OF_RSA, ": " + e.getMessage());
+        }
+
+        return bytes;
+    }
+
+
+    //////////////////////////////////////////////////////////
+    ///  Methods keys conversion
+    /////////////////////////////////////////////////////////
+    //-----Begin--------------------
+    @Nullable
+    public byte[] convertKeyToByte (Key key){
+        return key.getEncoded();
+    }
+
+    public Key convertByteToKey (byte[] bytes){
+        Key key;
+
+        try {
+
+            key = new SecretKeySpec(bytes, NAME_OF_RSA);
+            return key;
+        }catch (IllegalArgumentException e) {
+            Log.d(NAME_OF_RSA, ": " + e.getMessage());
+            return null;
+        }
+    }
+    //-----End----------------------
+
 }
